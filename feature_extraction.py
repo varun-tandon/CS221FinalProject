c9742ad19dc0d78080b85bdbab4e7c7efbe3edc1
varun-tandon <vrntandon@gmail.com>
2019-11-14 00:17:00 -0800

    removed cached files

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 
-df = pd.read_csv('data_parsed.csv')
+df = pd.read_csv('data_parsed_contains_word_features.csv')
 
 def contains_word_extractor(x):
     contains_word_features = {}
@@ -16,6 +16,26 @@ def contains_word_extractor(x):
         print("German, I cannot find this file: ", x['fuzzy_picked_file'])
     return x
 
+def count_num_words(x):
+    has_num_words = {}
+    with open('movie_scripts/' + x['ScriptLink']) as f:
+        text = f.read()
+        tokens = text.split()
+        for i in range(0, len(tokens), 100):
+            has_num_words['has_{}_num_words'.format(i)] = 1
+    x['num_words_features'] = json.dumps(has_num_words)
+    return x
+
+def count_num_chars(x):
+    has_num_chars = {}
+    with open('movie_scripts/' + x['ScriptLink']) as f:
+        text = f.read()
+        for i in range(0, len(text), 1000):
+            has_num_chars['has_{}_num_chars'.format(i)] = 1
+    x['num_chars_features'] = json.dumps(has_num_chars)
+    return x
 df = df.apply(contains_word_extractor, axis=1)
+df = df.apply(count_num_words, axis=1)
+df = df.apply(count_num_chars, axis=1)
 df = df.drop(columns=['Unnamed: 0'])
 df.to_csv('data_parsed_contains_word_features.csv')
