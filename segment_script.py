b868559997dbbd54dc9ef6fd729b11db32ccd10d
Gera Shabanets <gerashabanets@gmail.com>
2019-11-28 00:30:33 -0800

    trying another regex

--- segment_script.py
+++ segment_script.py
@@ -5,7 +5,7 @@ def main():
 	with open('movie_scripts/thor.html') as file:
 		script = file.readlines()
 		script = "".join(script)
-		print(script)
+		print(repr(script))
 		dialogue = segment(script)
 		print(dialogue)
 
@@ -14,8 +14,9 @@ def segment(x: str):
 	""" given a string that contains the script, this method finds all the dialogue """
 	#dialogue_regex = "(?:([A-Z]+ *[A-Z]+)\n).*?(?=$|([A-Z]+ *[A-Z]+)\n)"
 	#*** IN PROGRESS ***
-	dialogue_regex = "[A-Z]+\s+\w+\n"
-	d_list = re.findall(dialogue_regex, x)
+	dialogue_regex = "[A-Z]+.*\n(\s)+(\w|\n)+\n\n"
+	reg = "[A-Z]+.*\n\n"
+	d_list = re.findall(reg, repr(x))
 	return d_list
 
 
