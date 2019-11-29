e8f7808655fc60ff9cd6ae6506683aa887466d42
Gera Shabanets <gerashabanets@gmail.com>
2019-11-28 16:05:10 -0800

    improved dialogue extraction

--- segment_script.py
+++ segment_script.py
@@ -7,16 +7,18 @@ def main():
 		script = "".join(script)
 		print(repr(script))
 		dialogue = segment(script)
-		print(dialogue)
+		for entry in dialogue:
+			print(repr(entry))
+			print()
 
 
 def segment(x: str):
 	""" given a string that contains the script, this method finds all the dialogue """
 	#dialogue_regex = "(?:([A-Z]+ *[A-Z]+)\n).*?(?=$|([A-Z]+ *[A-Z]+)\n)"
 	#*** IN PROGRESS ***
-	dialogue_regex = "[A-Z]+.*\n(\s)+(\w|\n)+\n\n"
-	reg = "[A-Z]+.*\n\n"
-	d_list = re.findall(reg, repr(x))
+	dialogue_regex = r"[A-Z]+.*\n(\s)+(\w|\n)+\n\n"
+	reg = r"\s*([A-Z]+)\n(.*?)\s*(.*?)(\n)(?!\s)"
+	d_list = re.findall(reg, x, flags=re.DOTALL)
 	return d_list
 
 
